cce9f7d7f518601a9f1661fb158115803a8da42b
ShimeaG <shimeagbetsi@gmail.com>
2024-11-06 14:44:50 -0600

    Actually got the databases working

--- project/project/settings.py
+++ project/project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "historyFacts.apps.HistoryfactsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
