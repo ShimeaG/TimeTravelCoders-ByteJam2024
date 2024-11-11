75cd54e18ab046fb76f187a7bef0979a3cc73909
Josef Bausch <josef@hydras.us>
2024-11-11 14:13:06 -0600

    added button

--- frontend/src/app/admin/page.js
+++ frontend/src/app/admin/page.js
@@ -3,9 +3,31 @@ import AdminEventForm from "@/app/admin/EventForm.js";
 
 export default function Home() {
   return (
-    <div className={'pb-32 flex flex-col space-y-2 w-full'}>
-      <AdminYearForm/>
-      <AdminEventForm/>
-    </div>
-  );
+      <>
+          <div className={'fixed top-3 left-3 flex items-center'}>
+              <a href={'/'} className="flex items-center hover:underline">
+                  <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-5 h-5 mr-2"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      strokeWidth="2"
+                  >
+                      <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15 19l-7-7 7-7"
+                      />
+                  </svg>
+                  Back to Present
+              </a>
+          </div>
+          <div className={'pb-32 flex flex-col space-y-2 w-full'}>
+              <AdminYearForm/>
+              <AdminEventForm/>
+          </div>
+      </>
+  )
+      ;
 }
--- frontend/src/app/event/page.js
+++ frontend/src/app/event/page.js
@@ -27,26 +27,45 @@ export default function Event() {
   }, []);  // Empty dependency array means this runs once on component mount
 
   return (
-    <>
-      <div className={"pb-32 flex items-center justify-center pt-2 min-h-screen w-full flex-col flex-wrap"}>
-        {/* Map over the events array to render each Card with dynamic data */}
-        {events.length > 0 ? (
-          events.map((event) => (
-            <div key={event.event_id} className={"w-full flex justify-center"}>
-              <Card
-                name={event.event_name}
-                desc={event.event_desc}
-                img={event.event_img}
+      <>
+        <div className={'fixed top-3 left-3 flex items-center'}>
+          <a href={'/'} className="flex items-center hover:underline">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-5 mr-2"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+            >
+              <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 19l-7-7 7-7"
               />
-            </div>
-          ))
-        ) : (
-            <div className={'text-center'}>
-              <p>No events found for this year</p>
-              <a className={'hover:underline'} href={'/admin'}>{'Add your own \u2192'}</a>
-            </div>
-        )}
-      </div>
-    </>
+            </svg>
+            Back to Present
+          </a>
+        </div>
+        <div className={"pb-32 flex items-center justify-center pt-2 min-h-screen w-full flex-col flex-wrap"}>
+          {/* Map over the events array to render each Card with dynamic data */}
+          {events.length > 0 ? (
+              events.map((event) => (
+                  <div key={event.event_id} className={"w-full flex justify-center"}>
+                    <Card
+                        name={event.event_name}
+                        desc={event.event_desc}
+                        img={event.event_img}
+                    />
+                  </div>
+              ))
+          ) : (
+              <div className={'text-center'}>
+                <p>No events found for this year</p>
+                <a className={'hover:underline'} href={'/admin'}>{'Add your own \u2192'}</a>
+              </div>
+          )}
+        </div>
+      </>
   );
 }
